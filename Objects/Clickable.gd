eb417ccb4d357735ea29caf377fa86c5232a4572
Thibaut de la Chapelle <thibaut.de-la-chapelle@protonmail.com>
2019-05-19 16:30:57 +0200

    click: add distance check when clicking on objects

--- Objects/Clickable.gd
+++ Objects/Clickable.gd
@@ -2,9 +2,22 @@ extends Area2D
 
 signal clicked
 
+export var distance_check = true
+export var radius_check = 100
+
+func can_click(event: InputEventMouseButton) -> bool:
+	if not distance_check:
+		return true
+	var player := get_tree().get_nodes_in_group("Player")
+	if player.size() == 0:
+		return true
+	return ((player[0] as KinematicBody2D).position - event.position).length() < 50
+
 func _on_ClickableArea_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		event = event as InputEventMouseButton
+		if not can_click(event):
+			return
 		if event.button_index == BUTTON_LEFT and event.is_pressed():
 			emit_signal("clicked")
 
