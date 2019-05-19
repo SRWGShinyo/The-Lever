eb030303041a65d224c29ddcb0320910f0afb07a
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-05-19 19:24:37 +0200

    dog: now barking when pet.

--- Objects/Dog.gd
+++ Objects/Dog.gd
@@ -1,5 +1,21 @@
 extends Node2D
 
+var barks = []
+var rng = RandomNumberGenerator.new()
+var time = Timer.new()
+
+func _playRandomBark():
+	var toplay = rng.randi_range(0, len(barks) - 1)
+	var audio = barks[toplay]
+	audio.play()
+
+func _ready():
+	rng.randomize()
+	barks.append(MainTitleMusic.bark1)
+	barks.append(MainTitleMusic.bark2)
+	barks.append(MainTitleMusic.bark3)
+
+
 func _on_ClickableArea_clicked():
 	if $Clickable/ClickableArea/AnimatedSprite.animation == "Idle":
 		$Clickable/ClickableArea/AnimatedSprite.play("Pet")
@@ -8,6 +24,7 @@ func _on_ClickableArea_clicked():
 			return
 		var player := players[0] as KinematicBody2D
 		$Clickable/ClickableArea/AnimatedSprite.flip_h = player.position.x < position.x
+		_playRandomBark()
 
 func _on_AnimatedSprite_animation_finished():
 	if $Clickable/ClickableArea/AnimatedSprite.animation == "Pet":
