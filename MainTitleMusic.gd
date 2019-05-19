cb4b3016e990edecfe888583696384010c12d152
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-05-19 11:50:59 +0200

    music: modified music script so that it does what it has to do.

--- MainTitleMusic.gd
+++ MainTitleMusic.gd
@@ -1,14 +1,32 @@
 extends Control
 
-var audioMain;
-var audioLv1;
-var audioLv2;
+export var mx := 0
+export var mn := -55
+
+var audioMain: AudioStreamPlayer;
+var audioLv1: AudioStreamPlayer;
+var audioLv2: AudioStreamPlayer;
+
+var musiclvl : float = 50;
+var fxlvl : float = 50;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	audioMain = get_node("Audit")
 	audioLv1 = get_node("Level1")
 	audioLv2 = get_node("Level2")
+	
+	audioMain.volume_db = _computeDB(50)
+	audioLv1.volume_db = _computeDB(50)
+	audioLv2.volume_db = _computeDB(50)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _computeDB(valuetoSet : float):
+	return valuetoSet * (mx - mn)/100 + mn
+
+func changeMusicVolume():
+	audioMain.volume_db = _computeDB(musiclvl)
+	audioLv1.volume_db = _computeDB(musiclvl)
+	audioLv2.volume_db = _computeDB(musiclvl)
+	
+func changeFxVolume():
+	#FIXME: implement this for  any FX.
+	pass
