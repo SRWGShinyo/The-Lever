4750998ec0583138314032dc084752356975d119
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-05-19 15:58:26 +0200

    titlescreen: added cables towards lever to make the player understand that it connects things.

--- scenes/TitleScreen.gd
+++ scenes/TitleScreen.gd
@@ -1,9 +1,18 @@
 extends Control
 
 var sceneFade = 0
+var cableList = [];
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	cableList.append($PlayButton/CanvasLayer/cable)
+	cableList.append($Options/cable1)
+	cableList.append($Options/cable2)
+	cableList.append($CreditButton/cable1)
+	cableList.append($CreditButton/cable2)
+	cableList.append($CanvasLayer/CommandButton/CanvasLayer/cable)
+	
+	deactivateCables()
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -29,15 +38,42 @@ func _on_LeverRoot_clicked():
 	
 	if (sceneFade == 1):
 		MainTitleMusic.audioMain.stop()
-		$LeverRoot.set_scene_to_load("res://scenes/Level1.tscn")
+		$CanvasLayer/LeverRoot.set_scene_to_load("res://scenes/Level1.tscn")
 	
 	if (sceneFade == 2):
-		$LeverRoot.set_scene_to_load("res://scenes/Options.tscn")
+		$CanvasLayer/LeverRoot.set_scene_to_load("res://scenes/Options.tscn")
 	
 	if (sceneFade == 3):
-		$LeverRoot.set_scene_to_load("res://scenes/Credits.tscn")
+		$CanvasLayer/LeverRoot.set_scene_to_load("res://scenes/Credits.tscn")
 		
 	if (sceneFade == 4):
-		$LeverRoot.set_scene_to_load("res://scenes/Commands.tscn")
+		$CanvasLayer/LeverRoot.set_scene_to_load("res://scenes/Commands.tscn")
+
+func activateCable(var elem : Node):
+	elem.show()
+
+func deactivateCables():
+	for elem in cableList:
+		elem.hide()
+
+func _on_PlayButton_button_down():
+	deactivateCables()
+	activateCable($PlayButton/CanvasLayer/cable)
 
 
+func _on_Options_button_down():
+	deactivateCables()
+	activateCable($Options/cable1)
+	activateCable($Options/cable2)
+	pass # Replace with function body.
+
+
+func _on_CreditButton_button_down():
+	deactivateCables()
+	activateCable($CreditButton/cable1)
+	activateCable($CreditButton/cable2)
+
+
+func _on_CommandButton_button_down():
+	deactivateCables()
+	activateCable($CanvasLayer/CommandButton/CanvasLayer/cable)
