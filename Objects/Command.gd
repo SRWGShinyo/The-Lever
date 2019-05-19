9ba44e96e96571e9dbb0a3573995dd95e1676da1
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-05-19 21:19:17 +0200

    command: added plateforme control panel and command.

---
+++ Objects/Command.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var isvisible = false;
+
+func _ready():
+	$Tableau.set_process(false)
+	$Tableau.hide()
+	
+
+
+
+func _on_ClickableCommandArea_clicked():
+	MainTitleMusic.changeFxVolume($BipBoup)
+	if (isvisible):
+		$Tableau.set_process(false)
+		$Tableau.hide()
+		isvisible = false
+	
+	else:
+		$Tableau.set_process(true)
+		$Tableau.show()
+		isvisible = true
---
+++ Objects/Tableau.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+func _on_ClickableLeftArea_clicked():
+	MainTitleMusic.changeFxVolume($Click)
+
+func _on_ClickableUpArea_clicked():
+	MainTitleMusic.changeFxVolume($Click)
+
+func _on_ClickableRightArea_clicked():
+	MainTitleMusic.changeFxVolume($Click)
+
+func _on_ClickableDownArea_clicked():
+	MainTitleMusic.changeFxVolume($Click)
+
+func _on_ClickableOKArea_clicked():
+	MainTitleMusic.changeFxVolume($Click)
