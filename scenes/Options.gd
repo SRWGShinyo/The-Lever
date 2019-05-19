1a29623a7d1b02d8c5ed4cd1f2204441e8e5f15e
Yohann Degli Esposti <yohann.degli-esposti@epita.fr>
2019-05-19 11:51:41 +0200

    options: now having a working slider for overall music. not for fx.

--- scenes/Options.gd
+++ scenes/Options.gd
@@ -5,3 +5,19 @@ extends Control
 
 func _on_LeverRoot_clicked():
 	$LeverRoot.set_scene_to_load("res://scenes/TitleScreen.tscn")
+
+
+func _on_HSlider_value_changed(value):
+	var musicDisp = get_node("Music/DisplayVolume")
+	musicDisp.text = str(value) + "%"
+	
+	MainTitleMusic.musiclvl = value
+	MainTitleMusic.changeMusicVolume()
+
+
+func _on_FXSlider_value_changed(value):
+	var fxDisp = get_node("FX/DisplayFX")
+	fxDisp.text = str(value) + "%"
+	
+	MainTitleMusic.fxlvl = value
+	MainTitleMusic.changeFxVolume()
