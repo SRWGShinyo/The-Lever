accb0d7ff3477c361e4d7601c8de1225f0070c0e
Thibaut de la Chapelle <thibaut.de-la-chapelle@protonmail.com>
2019-05-19 17:53:27 +0200

    doggo: make doggo prefab

--- Objects/Dog.gd
+++ Objects/Dog.gd
@@ -1,12 +1,14 @@
 extends Node2D
 
-
 func _on_ClickableArea_clicked():
-	if $ClickableArea/AnimatedSprite.animation == "Idle":
-		$ClickableArea/AnimatedSprite.play("Pet")
-		var player := get_tree().get_nodes_in_group("Player")[0] as KinematicBody2D
-		$ClickableArea/AnimatedSprite.flip_h = player.position.x < position.x
+	if $Clickable/ClickableArea/AnimatedSprite.animation == "Idle":
+		$Clickable/ClickableArea/AnimatedSprite.play("Pet")
+		var players = get_tree().get_nodes_in_group("Player")
+		if players.size() == 0:
+			return
+		var player := players[0] as KinematicBody2D
+		$Clickable/ClickableArea/AnimatedSprite.flip_h = player.position.x < position.x
 
 func _on_AnimatedSprite_animation_finished():
-	if $ClickableArea/AnimatedSprite.animation == "Pet":
-		$ClickableArea/AnimatedSprite.play("Idle")
+	if $Clickable/ClickableArea/AnimatedSprite.animation == "Pet":
+		$Clickable/ClickableArea/AnimatedSprite.play("Idle")
